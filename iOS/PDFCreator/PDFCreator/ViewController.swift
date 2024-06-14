6d623d6fa5444c7c92f06984a3c80d06fd35d4aa
xulihang <xulihanghai@163.com>
2024-06-14 17:26:51 +0800

    iOS: basic layout

--- iOS/PDFCreator/PDFCreator/ViewController.swift
+++ iOS/PDFCreator/PDFCreator/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var selectImagesUIButton: UIButton!
+    
+    @IBOutlet weak var colorModeUIPickerView: UIPickerView!
+    @IBOutlet weak var enableAutoCroppingUISwitch: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func selectImagesUIButton_clicked(_ sender: Any) {
+        print("clicked")
+    }
+    
 }
 
