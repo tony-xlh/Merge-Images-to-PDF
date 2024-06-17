88e7b306a82a4daa364e68b073ec31659a1bfcf8
xulihang <xulihanghai@163.com>
2024-06-17 13:41:08 +0800

    iOS: add photo picker

--- iOS/PDFCreator/PDFCreator/ViewController.swift
+++ iOS/PDFCreator/PDFCreator/ViewController.swift
@@ -6,8 +6,26 @@
 //
 
 import UIKit
+import PhotosUI
 
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        for item in results {
+            print(item.itemProvider)
+            if (item.itemProvider.canLoadObject(ofClass: UIImage.self)) {
+                item.itemProvider.loadObject(ofClass: UIImage.self) { image , error  in
+                    if let error{
+                        print(error)
+                    }
+                    if let selectedImage = image as? UIImage{
+                        print(selectedImage.size)
+                    }
+                }
+            }
+        }
+        picker.dismiss(animated: true)
+    }
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
@@ -41,7 +59,14 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
 
     @IBAction func selectImagesUIButton_clicked(_ sender: Any) {
         print("clicked")
+
+        var configuration = PHPickerConfiguration(photoLibrary: .shared())
+        //0 - unlimited 1 - default
+        configuration.selectionLimit = 0
+        configuration.filter = .images
+        let pickerViewController = PHPickerViewController(configuration: configuration)
+        pickerViewController.delegate = self
+        present(pickerViewController, animated: true)
     }
-    
 }
 
