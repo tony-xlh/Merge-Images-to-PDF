9a6fca439ce147ac91bb23e20e22b2f37687dcdd
xulihang <xulihanghai@163.com>
2024-06-28 15:42:12 +0800

    update viewer's size

--- Salesforce/DocumentManager/force-app/main/default/lwc/documentManager/documentManager.js
+++ Salesforce/DocumentManager/force-app/main/default/lwc/documentManager/documentManager.js
@@ -1,3 +1,24 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
+import ddv from '@salesforce/resourceUrl/ddv';
 
-export default class DocumentManager extends LightningElement {}
+export default class DocumentManager extends LightningElement {
+  ddvInitialized = false;
+  ddvFrame;
+  
+  renderedCallback() {
+      if (this.ddvInitialized) {
+          return;
+      }
+      this.ddvInitialized = true;
+
+      this.ddvFrame = document.createElement('iframe');
+      this.ddvFrame.src = ddv + "/index.html";
+      // div tag in which iframe will be added should have id attribute with value myDIV
+      this.template.querySelector("div.ddvViewer").appendChild(this.ddvFrame);
+
+      // provide height and width to it
+      this.ddvFrame.setAttribute("style","height:100%;width:100%;");
+  }
+}
