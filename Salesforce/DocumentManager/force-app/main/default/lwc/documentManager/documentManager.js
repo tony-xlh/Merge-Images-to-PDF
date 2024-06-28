08aa83028496b3524bae85a08a7027dfe285c190
xulihang <xulihanghai@163.com>
2024-06-28 16:10:40 +0800

    enable communication via postMessage

--- Salesforce/DocumentManager/force-app/main/default/lwc/documentManager/documentManager.js
+++ Salesforce/DocumentManager/force-app/main/default/lwc/documentManager/documentManager.js
@@ -6,13 +6,30 @@ import ddv from '@salesforce/resourceUrl/ddv';
 export default class DocumentManager extends LightningElement {
   ddvInitialized = false;
   ddvFrame;
+  dataURL = "";
+
+  get dataURLLabel() {
+    if (this.dataURL.length > 15) {
+      return this.dataURL.substring(0,10) + "...";
+    }else{
+      return this.dataURL;
+    }
+  }
   
   renderedCallback() {
       if (this.ddvInitialized) {
           return;
       }
       this.ddvInitialized = true;
-
+      window.addEventListener(
+        "message",
+        (event) => {
+          console.log(event);
+          console.log("received message in lwc");
+          this.dataURL = event.data;
+        },
+        false,
+      );
       this.ddvFrame = document.createElement('iframe');
       this.ddvFrame.src = ddv + "/index.html";
       // div tag in which iframe will be added should have id attribute with value myDIV
@@ -20,5 +37,9 @@ export default class DocumentManager extends LightningElement {
 
       // provide height and width to it
       this.ddvFrame.setAttribute("style","height:100%;width:100%;");
+  }
+
+  merge(){
+    this.ddvFrame.contentWindow.postMessage("merge");
   }
 }
